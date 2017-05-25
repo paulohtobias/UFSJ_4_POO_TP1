4a193c40ab6a58df4f05666628f9939128bbe805
Paulo Tobias <paulohtobias@outlook.com>
2017-05-24 21:18:36 -0300

    VERSÃO FINAL

--- src/Mapa/Sala.java
+++ src/Mapa/Sala.java
@@ -19,18 +19,61 @@ import java.util.Random;
  * @author paulo
  */
 public class Sala {
-    private int id;
+    /**
+     * Número identificador da sala.
+     */
+    private final int id;
+    
+    /**
+     * Porta A da sala.
+     */
     private Porta portaA = null;
+    
+    /**
+     * Porta B da sala.
+     */
     private Porta portaB = null;
+    
+    /**
+     * Porta C da sala.
+     */
     private Porta portaC = null;
     
-    private ArrayList<Item> itens;
+    /**
+     * Lista de itens que estão na sala.
+     */
+    private final ArrayList<Item> itens;
     
+    /**
+     * Construtor de Sala. Cria uma sala vazia e adiciona alguns itens aleatórios
+     * 
+     * @param id número identificador da sala
+     */
     public Sala(int id){
         this.id = id;
         this.itens = new ArrayList<>();
         this.setItens();
     }
+    
+    /**
+     * Liga a sala à {@code sala2} pela porta {@code porta1_id}. Precisa ser
+     * chamada a partir da sala 2 para conectar-se com a sala 1. Esta função
+     * garante que ambas as salas compartilhem a mesma porta. Portanto, trancar
+     * a porta da sala 1 que a conecta à sala 2 significa que a porta da sala 2
+     * que dá acesso à sala 1 também estará trancada sem precisar de código
+     * adicional.
+     * <br>
+     * Exemplo: {@code sala1.Conectar("A", sala5)} conectará a sala 1 à sala 5
+     * através da porta A da sala A. Quanto à sala 5, se a porta que a conecta
+     * à sala 1 já existia, então as duas portas são associadas. Caso contrário,
+     * uma nova porta é criada e o acesso é possível apenas da sala 1 para a 5.
+     * Para ligar a sala 5 à sala 1, é preciso chamar a função a partir da sala
+     * 5 e especificar qual porta da sala 5 levará à sala 1
+     * 
+     * @param porta1_id qual porta da sala estará conectada à {@code sala2}.
+     *                  Pode ser "A", "B", ou "C"
+     * @param sala2 qual sala a {@code porta1_id} dará acesso
+     */
     public void Conectar(String porta1_id, Sala sala2){
         switch(porta1_id){
             case "A":
@@ -57,10 +100,21 @@ public class Sala {
         }
     }
     
+    /**
+     * Retorna o número identificador da sala.
+     * 
+     * @return {@code id}
+     */
     public int getId(){
         return this.id;
     }
     
+    /**
+     * Verifica se {@code str_item} existe na sala e o retorna.
+     * 
+     * @param str_item {@code String} representando o item desejado
+     * @return Item encontrado. Retorna {@code null} se não encontrar o item
+     */
     public Item getItem(String str_item){
         //str_item = str_item.toLowerCase();
         for(Item item : this.itens){
@@ -71,24 +125,52 @@ public class Sala {
         return null;
     }
     
+    /**
+     * Adiciona um item na sala.
+     * 
+     * @param item Item para ser adicionado na sala
+     */
     public void addItem(Item item){
         if(item != null){
             this.itens.add(item);
         }
     }
     
+    /**
+     * Porta A da sala.
+     * 
+     * @return {@code portaA}
+     */
     public Porta getPortaA() {
         return this.portaA;
     }
 
+    /**
+     * Porta B da sala. Pode ser {@code null}
+     * 
+     * @return {@code portaB}
+     */
     public Porta getPortaB() {
         return this.portaB;
     }
 
+    /**
+     * Porta C da sala. Pode ser {@code null}
+     * 
+     * @return {@code portaC}
+     */
     public Porta getPortaC() {
         return this.portaC;
     }
     
+    /**
+     * Retorna a porta de acordo com {@code str_porta}.
+     * 
+     * @param str_porta {@code String} string no formato do jogo. Exemplo:
+     *                  "a door", "b door" ou "c door"
+     * @return A porta correspondente à {@code str_porta}. Ou {@code null} caso
+     *         {@code str_porta} seja uma string inválida.
+     */
     public Porta getPorta(String str_porta){
         if(str_porta.equals("a door")){
             return this.portaA;
@@ -101,6 +183,13 @@ public class Sala {
         }
         return null;
     }
+    
+    /**
+     * Retorna a porta que conecta à sala {@code sala2_id}, caso esta exista.
+     * 
+     * @param sala2_id id de alguma sala adjacente
+     * @return a Porta que conecta as duas salas ou {@code null} caso esta não exista
+     */
     public Porta getPorta(int sala2_id){
         if(this.portaA != null){
             if(this.portaA.getSala2(this.id) == sala2_id){
@@ -120,6 +209,11 @@ public class Sala {
         return null;
     }
     
+    /**
+     * Escolhe uma porta aleatória (não nula) da sala e retorna sua string correspondente.
+     * 
+     * @return "A door", "B door" ou "C door" escolhido aleatóriamente
+     */
     public String getPortaAleatoria(){
         Random rand = new Random();
         int cod_porta = rand.nextInt(3);
@@ -138,6 +232,12 @@ public class Sala {
         return "A door";
     }
     
+    /**
+     * Insere um máximo de 5 itens aleatóriamente na sala. Os itens podem ser
+     * {@code Ouro}, {@code Diamante}, {@code Pocao} e {@code Machado}. Chaves
+     * não são adicionadas aqui pois o local dessas não pode ser definido
+     * aleatoriamente.
+     */
     private void setItens(){
         Random rand = new Random();
         int maxItens = 5;
@@ -162,6 +262,15 @@ public class Sala {
             }
         }
     }
+    
+    /**
+     * Adiciona chaves na sala.
+     * 
+     * @param chaves_str {@code String} no formato "(s1-s2,s6-s19,...)" onde
+     * s<i>i</i>-s<i>j</i> é a chave da porta que liga as salas <i>i</i> e <i>j</i>.
+     * As chaves são separadas por vírgula e não tem limite de quantas chaves
+     * podem ter na sala. "(,)" significa que a sala não tem chaves.
+     */
     public void addChaves(String chaves_str){
         int chaves_tamI = chaves_str.indexOf('(') + 1;
         int chaves_tamF = chaves_str.indexOf(')', chaves_tamI);
@@ -174,10 +283,19 @@ public class Sala {
         }
     }
     
+    /**
+     * Remove o item da sala.
+     * 
+     * @param item Item para ser removido da sala
+     */
     public void removerItem(Item item){
         this.itens.remove(item);
     }
     
+    /**
+     * Mostra na tela as informações da sala, como {@code id}, portas (e para
+     * onde levam) e itens.
+     */
     public void Listar(){
         System.out.printf("Sala %d\n", this.id);
         System.out.print("  Portas:");
--- src/Personagem/Personagem.java
+++ src/Personagem/Personagem.java
@@ -44,26 +44,56 @@ public class Personagem {
      */
     protected Porta proximaPorta;
     
-    public Personagem(String id, int maxItens){
-        this.nome = id;
+    /**
+     * Construtor da Classe Personagem. Cria um novo personagem com {@code nome}
+     * e cria um inventário que suporta até {@code maxItens}.
+     * 
+     * @param nome nome identificador do personagem
+     * @param maxItens número máximo de itens no inventário
+     */
+    public Personagem(String nome, int maxItens){
+        this.nome = nome;
         this.maxItens = maxItens;
         this.itens = new ArrayList<>();
         this.proximoItem = null;
         this.proximaPorta = null;
     }
 
+    /**
+     * Retorna o nome do personagem.
+     * 
+     * @return {@code nome}
+     */
     public String getNome() {
         return this.nome;
     }
     
+    /**
+     * Retorna a capacidade máxima do inventário.
+     * 
+     * @return {@code maxItens}
+     */
     public int getMaxItens() {
         return this.maxItens;
     }
 
+    /**
+     * Retorna o inventário do personagem.
+     * 
+     * @return {@code ArrayList<Item>} com todos os itens no inventário do
+     *         personagem.
+     */
     public ArrayList<Item> getItens() {
         return this.itens;
     }
     
+    /**
+     * Retorna o item cujo nome é correspondente à {@code item_str}, caso exista
+     * um no inventário. Pode retornar {@code null}.
+     * 
+     * @param item_str {@code String} a ser comparada com cada item do inventário.
+     * @return o {@code Item} que foi encontrado ou {@null}
+     */
     public Item getItem(String item_str){
         for(Item item: this.getItens()){
             if(item.toString().startsWith(item_str)){
@@ -73,32 +103,69 @@ public class Personagem {
         return null;
     }
     
+    /**
+     * Retorna a sala atual do personagem.
+     * 
+     * @return {@code salaAtual}
+     */
     public int getSalaAtual() {
         return salaAtual;
     }
     
+    /**
+     * Retorna o Item ao qual o personagem está próximo.
+     * 
+     * @return {@code Item} ou {@code null}, caso o personagem não esteja próximo
+     *         a um item
+     */
     public Item getProximoItem(){
         return this.proximoItem;
     }
     
+    /**
+     * Retorna a Porta ao qual o personagem está próximo.
+     * 
+     * @return {@code Porta} ou {@code null}, caso o personagem não esteja próximo
+     *         a uma porta
+     */
     public Porta getProximaPorta(){
         return this.proximaPorta;
     }
     
+    /**
+     * Altera a sala do personagem.
+     * 
+     * @param salaAtual nova sala para qual o personagem estará
+     */
     public void setSalaAtual(int salaAtual){
         this.salaAtual = salaAtual;
     }
 
+    /**
+     * Move o personagem para perto de um item.
+     * @param item item que estará próximo ao personagem
+     */
     public void Mover(Item item){
         this.proximoItem = item;
         this.proximaPorta = null;
     }
     
+    /**
+     * Move o personagem para perto de uma porta
+     * @param porta porta que estará próximo ao personagem
+     */
     public void Mover(Porta porta){
         this.proximaPorta = porta;
         this.proximoItem = null;
     }
 
+    /**
+     * Pega o item que está próximo ao personagem, caso haja algum e o
+     * inventário aceite mais um item.
+     * 
+     * @return o {@code Item} que foi pego. Retorna {@code null} se não
+     *         for possível pegar
+     */
     public Item Pegar(){
         if( (this.itens.size() >= this.getMaxItens()) || (this.getProximoItem() == null) ){
             return null;
@@ -110,16 +177,33 @@ public class Personagem {
         return item;
     }
     
+    /**
+     * Remove o item do inventário do personagem.
+     * 
+     * @param item a ser removido
+     */
     public void Largar(Item item){
         this.itens.remove(item);
     }
     
+    /**
+     * Remove e retorna o item do inventário do personagem.
+     * 
+     * @param str_item {@code String} representando o item
+     * @return O item removido. Pode ser {@code null}
+     */
     public Item Largar(String str_item){
         Item item = this.getItem(str_item);
         this.itens.remove(item);
         return item;
     }
     
+    /**
+     * Move o personagem de uma sala para outra. O movimento só é feito se
+     * o personagem está perto de uma porta e esta está aberta.
+     * 
+     * @return {@code true} se foi possível sair da porta
+     */
     public Boolean Sair(){
         if(this.getProximaPorta() == null){
             return false;
